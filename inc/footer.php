9410b5a98fd7bbca778f6e94dadcf720daf77e44
MaKnok <marinaknok@gmail.com>
2019-01-19 17:09:24 -0200

    First commit

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+
+	<!-- UIkit CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.26/css/uikit.min.css" />
+
+	<!-- UIkit JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.26/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.26/js/uikit-icons.min.js"></script>
+
