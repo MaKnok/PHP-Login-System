9147b0b7a213b053b85fc8c8526f6acfde58a318
MaKnok <marinaknok@gmail.com>
2019-01-19 17:08:41 -0200

    First commit

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	//if there's no constant defined called '__CONFIG__', does not load the file
+	if (!defined('__CONFIG__')){
+
+		exit('You haven\'t defined config');
+	
+	}
+
+	//our config is below
+
+
+?>
